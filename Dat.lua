5bc2d13062633ec72770bb6d3e0803fffc8978e2
baoprobelike <143244670+baoprobelike@users.noreply.github.com>
2025-02-18 20:24:48 +0700

    randomcode
    1

---
+++ Dat.lua
@@ -0,0 +1,31 @@
+Is.Start = gui.Lua.StartService
+
+text = '('BAOLARA')
+Window.Config://
+Connect:127.127.68.68
+getCloudService = 127.127.68.68:1000
+
+ Connect.to.RobloxAPI = CSharp.core
+
+      GetGenv().install.get.FileService = Core2sharp.dll
+
+
+                   User.START.lua = true
+                   CheckFuction:()
+--Gui2Lua then sub to me :)
+
+
+     Env.GetWebSource = api.roblox.com/v2/roblox/version-api/version-f9977b146957492c.ver/?locale=en_GB
+        Is.Data = asset2.baolara.natutool.org/key/version-1959-57492c/bypassed.link/
+
+             FileSource:
+
+ [File.unk###]
+getFile = GithubTool
+
+isDone = TabClosed.Service = true
+
+  ConnectToWorkspace = true
+
+      end)
+     
